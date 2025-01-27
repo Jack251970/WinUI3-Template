a63f4f8fb2cfe287350188f0b60853e951264fa9
Jack251970 <1160210343@qq.com>
2025-01-27 19:05:28 +0800

    add event handler demo

--- WinUI3Demo/ViewModels/Pages/HomeViewModel.cs
+++ WinUI3Demo/ViewModels/Pages/HomeViewModel.cs
@@ -4,13 +4,27 @@ using CommunityToolkit.Mvvm.Input;
 
 namespace WinUI3Demo.ViewModels.Pages;
 
-public partial class HomeViewModel : ObservableRecipient
+public partial class HomeViewModel : ObservableRecipient, INavigationAware
 {
     [ObservableProperty]
     private string _appDisplayName = ConstantHelper.AppDisplayName;
 
     [ObservableProperty]
     private List<Vector2> _salesData;
+
+    [ObservableProperty]
+    private int _sSS;
+
+    private bool changing;
+
+    partial void OnSSSChanged(int value)
+    {
+        if (changing) return;
+
+        changing = true;
+        SSS = SSS + 1;
+        changing = false;
+    }
 
     public HomeViewModel()
     {
@@ -21,6 +35,16 @@ public partial class HomeViewModel : ObservableRecipient
             new Vector2() { X = 59f, Y = 2400f },
             new Vector2() { X = 98f, Y = 4800f },
         ];
+    }
+
+    public void OnNavigatedTo(object parameter)
+    {
+        Console.WriteLine("OnNavigatedTo");
+    }
+
+    public void OnNavigatedFrom()
+    {
+        Console.WriteLine("OnNavigatedFrom");
     }
 
     partial void OnSalesDataChanged(List<Vector2> value)
--- WinUI3Demo/Views/Pages/HomePage.xaml.cs
+++ WinUI3Demo/Views/Pages/HomePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 
 namespace WinUI3Demo.Views.Pages;
 
@@ -10,5 +12,47 @@ public sealed partial class HomePage : Page
     {
         ViewModel = DependencyExtensions.GetRequiredService<HomeViewModel>();
         InitializeComponent();
+
+        // Slider control is handling the pointer input internally and marking it as handled
+        // So we need to explicitly register the event with AddHandler and set the handledEventsToo parameter to true
+        Slider.AddHandler(PointerReleasedEvent, new PointerEventHandler(Slider_PointerReleased), true);
+
+        ContentArea.Loaded += ContentArea_Loaded;
+        ContentArea.Unloaded += ContentArea_Unloaded;
+
+        Loaded += HomePage_Loaded;
+        Unloaded += HomePage_Unloaded;
+
+        Console.WriteLine("HomePage Constructor");
+    }
+
+    private void HomePage_Loaded(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("HomePage_Loaded");
+    }
+
+    private void HomePage_Unloaded(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("HomePage_Unloaded");
+    }
+
+    private void ContentArea_Loaded(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("ContentArea_Loaded");
+    }
+
+    private void ContentArea_Unloaded(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("ContentArea_Unloaded");
+    }
+
+    private void Slider_KeyUp(object sender, KeyRoutedEventArgs e)
+    {
+
+    }
+
+    private void Slider_PointerReleased(object sender, PointerRoutedEventArgs e)
+    {
+
     }
 }
